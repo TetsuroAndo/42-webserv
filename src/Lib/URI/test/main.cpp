c143221819ea70a97fdf1dca9a651e268b69c543
TsunamiSaito <jvlipnitsk@gmail.com>
2025-09-08 17:45:04 +0900

    WIP on tsunami: c60c59f サーバーの大枠を作成

--- src/Lib/URI/test/main.cpp
+++ src/Lib/URI/test/main.cpp
@@ -1,28 +1,28 @@
-#include "../uri.hpp"
-#include <iostream>
-
-#include <iterator>
-
-int main(int ac, char **av) {
-	if (ac != 2)
-		return 1;
-	std::string mode(av[1]);
-	// 重複している行を削除し、正しい構文で書き直す
-	std::string input((std::istreambuf_iterator<char>(std::cin)),
-					  std::istreambuf_iterator<char>());
-
-	std::string out;
-	if (mode == "encodeURI") {
-		out = URI::encodeURI(input);
-	} else if (mode == "decodeURI") {
-		out = URI::decodeURI(input);
-	} else if (mode == "encodeURIComponent") {
-		out = URI::encodeURIComponent(input);
-	} else if (mode == "decodeURIComponent") {
-		out = URI::decodeURIComponent(input);
-	} else {
-		return 1;
-	}
-	std::cout << out;
-	return 0;
-}
+//#include "../uri.hpp"
+//#include <iostream>
+//
+//#include <iterator>
+//
+//int main(int ac, char **av) {
+//	if (ac != 2)
+//		return 1;
+//	std::string mode(av[1]);
+//	// 重複している行を削除し、正しい構文で書き直す
+//	std::string input((std::istreambuf_iterator<char>(std::cin)),
+//					  std::istreambuf_iterator<char>());
+//
+//	std::string out;
+//	if (mode == "encodeURI") {
+//		out = URI::encodeURI(input);
+//	} else if (mode == "decodeURI") {
+//		out = URI::decodeURI(input);
+//	} else if (mode == "encodeURIComponent") {
+//		out = URI::encodeURIComponent(input);
+//	} else if (mode == "decodeURIComponent") {
+//		out = URI::decodeURIComponent(input);
+//	} else {
+//		return 1;
+//	}
+//	std::cout << out;
+//	return 0;
+//}
