50b64f6c4f1d5a193b59de53ee881e24777621c3
TetsuroAndo <tetsuro997@gmail.com>
2025-09-17 14:02:24 +0900

    fix: CPP98

--- src/Http/Core/HttpStatus.cpp
+++ src/Http/Core/HttpStatus.cpp
@@ -2,9 +2,51 @@
 #include <string>
 #include <map>
 
-const std::string HttpStatus::getReason(int code) {
-	std::map<int, std::string>::const_iterator it = statusReasons.find(code);
+namespace {
+	typedef std::map<int, std::string> StatusMap;
+	StatusMap createStatusReasons() {
+		StatusMap code;
+		// Informational
+		code[100] = "Continue";
+		// Success
+		code[200] = "OK";
+		code[201] = "Created";
+		code[202] = "Accepted";
+		code[204] = "No Content";
+		// Redirection
+		code[301] = "Moved Permanently";
+		code[302] = "Found";
+		code[303] = "See Other";
+		// Client Error
+		code[400] = "Bad Request";
+		code[401] = "Unauthorized";
+		code[403] = "Forbidden";
+		code[404] = "Not Found";
+		code[405] = "Method Not Allowed";
+		code[408] = "Request Timeout";
+		code[413] = "Payload Too Large";
+		code[414] = "URI Too Long";
+		code[415] = "Unsupported Media Type";
+		code[431] = "Request Header Fields Too Large";
+		/// Server Error
+		code[500] = "Internal Server Error";
+		code[501] = "Not Implemented";
+		code[502] = "Bad Gateway";
+		code[503] = "Service Unavailable";
+		code[504] = "Gateway Timeout";
+		code[505] = "HTTP Version Not Supported";
+		return code;
+	}
+
+	// Static
+	const StatusMap statusReasons = createStatusReasons();
+	const std::string unknown = "Internal Server Error";
+
+} // namespace
+
+const std::string &HttpStatus::getReason(int code) {
+	StatusMap::const_iterator it = statusReasons.find(code);
 	if (it != statusReasons.end())
 		return it->second;
-	return "Internal Server Error";
+	return unknown;
 }
--- src/Http/Core/HttpStatus.hpp
+++ src/Http/Core/HttpStatus.hpp
@@ -5,37 +5,13 @@
 
 namespace HttpStatus {
 
-	static const std::map<int, std::string> statusReasons = {
-		{100, "Continue"},
-		// Success
-		{200, "OK"},
-		{201, "Created"},
-		{202, "Accepted"},
-		{204, "No Content"},
-		// Redirection
-		{301, "Moved Permanently"},
-		{302, "Found"},
-		{303, "See Other"},
-		// Client Error
-		{400, "Bad Request"},
-		{401, "Unauthorized"},
-		{403, "Forbidden"},
-		{404, "Not Found"},
-		{405, "Method Not Allowed"},
-		{408, "Request Timeout"},
-		{413, "Payload Too Large"},
-		{414, "URI Too Long"},
-		{415, "Unsupported Media Type"},
-		{431, "Request Header Fields Too Large"},
-		// Server Error
-		{500, "Internal Server Error"},
-		{501, "Not Implemented"},
-		{502, "Bad Gateway"},
-		{503, "Service Unavailable"},
-		{504, "Gateway Timeout"},
-		{505, "HTTP Version Not Supported"}
-	};
-
-	const std::string getReason(int code);
+	/**
+	 * @brief HTTPステータスコードに対応する理由フレーズを返します。
+	 *
+	 * @param code HTTPステータスコード (e.g., 200, 404).
+	 * @return const std::string& 理由フレーズ (e.g., "OK", "Not Found").
+	 * 見つからない場合は "Internal Server Error" のフレーズを返します。
+	 */
+	const std::string &getReason(int code);
 
 } // namespace HttpStatus
