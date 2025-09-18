71ceaac86e34a9fca087511afd36b3a44baf1cd3
Tetsuro Ando <tetsuro997@gmail.com>
2025-09-18 23:24:11 +0900

    fix: uri indent

--- src/Http/URI/URI.cpp
+++ src/Http/URI/URI.cpp
@@ -54,7 +54,7 @@ std::string URI::decodeURI(const std::string &str) {
 			std::size_t expected_cont = 0;
 			if ((first & 0x80) == 0x00) { // 1-byte sequence (ASCII)
 				expected_cont = 0;
-	} else if ((first & 0xE0) == 0xC0) { // 2-byte sequence
+			} else if ((first & 0xE0) == 0xC0) { // 2-byte sequence
 				expected_cont = 1;
 			} else if ((first & 0xF0) == 0xE0) { // 3-byte sequence
 				expected_cont = 2;
