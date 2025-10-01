dfa67d6dad5fe6a28ac3bdcb19bacc8ce2127c27
TetsuroAndo <tetsuro997@gmail.com>
2025-10-01 20:48:48 +0900

    ServerでLogging設定を行う実装を追加

---
+++ src/Server/Logging/Logging.cpp
@@ -0,0 +1,72 @@
+#include "Logging.hpp"
+#include "../Lib/Logger/AccessLog/AccessLogger.hpp"
+#include "../Lib/Logger/ErrorLog/Logger.hpp"
+#include <vector>
+
+namespace {
+
+/**
+ * @brief アクセスログが無効化されているかをチェックする
+ * @return true 無効化されている, false 有効なログ設定が存在する
+ */
+bool isAccessLoggingDisabled(const std::vector<AccessLog>& logs) {
+	for (std::vector<AccessLog>::const_iterator it = logs.begin(); it != logs.end(); ++it) {
+		if (it->isDisable) {
+			return true;
+		}
+	}
+	return false;
+}
+
+/**
+ * @brief エラーログが無効化されているかをチェックする
+ * @return 無効化されている場合はtrue、そうでなければfalse
+ */
+bool isErrorLoggingDisabled(const std::vector<ErrorLog>& logs) {
+	for (std::vector<ErrorLog>::const_iterator it = logs.begin(); it != logs.end(); ++it) {
+		if (it->isDisable) {
+			return true;
+		}
+	}
+	return false;
+}
+} // namespace
+
+namespace Logging {
+
+/**
+ * @brief ロガーを設定する
+ * @param config 設定情報
+ */
+void setupLoggers(const Config& config) {
+	const std::vector<AccessLog>& accessLogs = config.getAccessLogs();
+	if (!isAccessLoggingDisabled(accessLogs)) {
+
+		AccessLogger& accessLogger = AccessLogger::getInstance();
+
+		std::vector<AccessLog>::const_iterator it;
+		for (it = accessLogs.begin(); it != accessLogs.end(); ++it) {
+			if (it->sink == File) {
+				accessLogger.setSinkFile(it->logDir, it->filename, it->format, it->maxFileSize, it->maxBackupFiles);
+			} else if (it->sink == Console) {
+				accessLogger.setSinkConsole(it->format);
+			}
+		}
+	}
+
+	const std::vector<ErrorLog>& errorLogs = config.getErrorLogs();
+	if (!isErrorLoggingDisabled(errorLogs)) {
+
+		Logger& errorLogger = Logger::getInstance();
+
+		std::vector<ErrorLog>::const_iterator it;
+		for (it = errorLogs.begin(); it != errorLogs.end(); ++it) {
+			if (it->sink == File) {
+				errorLogger.setSinkFile(it->logDir, it->filename, it->format, it->level, it->filterMode, it->maxFileSize, it->maxBackupFiles);
+			} else if (it->sink == Console) {
+				errorLogger.setSinkConsole(it->format, it->level, it->filterMode);
+			}
+		}
+	}
+}
+} // namespace Logging
---
+++ src/Server/Logging/Logging.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "../Config/Config.hpp"
+
+namespace Logging {
+void setupLoggers(const Config& config);
+}
