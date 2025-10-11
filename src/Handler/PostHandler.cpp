05c67b764680134f2274000863c1af4669f00099
Tomoki Sato <tomokirenrakuyo@gmail.com>
2025-10-11 20:30:27 +0900

    feat: PostHandlerのファイル名生成にハイフンを追加し、ログ出力を改善

--- src/Handler/PostHandler.cpp
+++ src/Handler/PostHandler.cpp
@@ -20,13 +20,13 @@ PostHandler::~PostHandler() {
 }
 
 namespace {
-std::string removeSpaceCoronComma(const std::string &str) {
+std::string removeSpaceCoronCommaHyphen(const std::string &str) {
 	std::string result;
 	result.reserve(str.size());
 
 	for (std::string::size_type i = 0; i < str.size(); ++i) {
 		const char c = str[i];
-		if (c != ' ' && c != ':' && c != ',') {
+		if (c != ' ' && c != ':' && c != ',' && c != '-') {
 			result += c;
 		}
 	}
@@ -42,6 +42,7 @@ HttpResponse PostHandler::handle(const HttpRequest &req, const Config &config) {
 
 	const std::string filePath =
 		HandlerUtil::resolvePath(req.getPath(), config);
+	LOG(DEBUG) << "PostHandler resolved path: " << filePath;
 	LOG(DEBUG) << "Called!: \n"
 		<< "    req head : "
 		<< (req.getHeader("Content-Type").empty()
@@ -52,13 +53,23 @@ HttpResponse PostHandler::handle(const HttpRequest &req, const Config &config) {
 		<< (req.getBody().empty() ? 0 : req.getBody().size()) << "\n"
 		<< "    req name : "
 		<< (req.getPath().empty() ? "empty" : req.getPath()) << "\n";
-	// TODO : req.getPath()がlocationに一致しているかを検索する
-	// curl -X POST http://127.0.0.1:8080/ -H "Content-Type: image/png" --data-binary "@./tmp/img.png"
 
+	// curl -X POST http://127.0.0.1:8080/ -H "Content-Type: image/png" --data-binary "@./tmp/img.png"
 	// 上記のコマンドを、@./tmp/img.pngを用意した状態でdefault.yamlでサーバーを起動すると動く
 
 	const Location &loc = config.getLocation(req.getPath());
+	// TODO : req.getPath()がlocationに一致しているかを検索する
+	if (req.getPath() != loc.path) {
+		LOG(INFO) << "Requested path does not match actual path";
+		HandlerUtil::generateErrorBody(req.getMethod(), response,
+		                               HttpStatus::NOT_FOUND);
+		return response;
+	}
+
+	LOG(DEBUG) << "loc path : " << loc.path;
+
 	const std::string uploadStore = loc.uploadStore;
+	LOG(DEBUG) << "uploadStore: " << uploadStore;
 	// uploadする場所が指定されていない
 	if (loc.uploadStore.empty()) {
 		LOG(ERROR) << "Upload store is empty";
@@ -69,7 +80,8 @@ HttpResponse PostHandler::handle(const HttpRequest &req, const Config &config) {
 	struct stat s;
 	// upload storeが存在しない
 	if (stat(uploadStore.c_str(), &s) != 0 || !S_ISDIR(s.st_mode)) {
-		LOG(ERROR) << "PostHandler : Upload Store \"" << uploadStore << "\" is not exist.";
+		LOG(ERROR) << "PostHandler : Upload Store \"" << uploadStore <<
+ "\" is not exist.";
 		std::cout << uploadStore.c_str() << std::endl;
 		HandlerUtil::generateErrorBody(req.getMethod(), response,
 		                               HttpStatus::INTERNAL_SERVER_ERROR);
@@ -79,14 +91,19 @@ HttpResponse PostHandler::handle(const HttpRequest &req, const Config &config) {
 		MimeType::getExtension(req.getHeader("Content-Type"));
 	// このサーバーで処理できないMimeType
 	if (expansion.empty()) {
-		LOG(ERROR) << "PostHandler : This Content-Type is Not Supported";
+		LOG(INFO) << "PostHandler : This Content-Type is Not Supported";
 		HandlerUtil::generateErrorBody(req.getMethod(), response,
 		                               HttpStatus::INTERNAL_SERVER_ERROR);
 		return response;
 	}
 	// TODO:getTokenのcharsetを変える
-	std::string target = uploadStore + "/" + Token::genToken(32) + "_" +
-	                     removeSpaceCoronComma(TimeCache::getCurrentTime()) +
+	std::string target = uploadStore + "/" +
+	                     removeSpaceCoronCommaHyphen(TimeCache::getGmtDate()) + "-" + removeSpaceCoronCommaHyphen(TimeCache::getGmtTime()) +
+	                     "_" +
+	                     Token::genToken(
+		                     8,
+		                     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+	                     +
 	                     expansion;
 	std::ofstream file(target.c_str());
 	// ファイル作成失敗
@@ -101,6 +118,7 @@ HttpResponse PostHandler::handle(const HttpRequest &req, const Config &config) {
 	file.close();
 	HandlerUtil::generateErrorBody(req.getMethod(), response,
 	                               HttpStatus::CREATED);
-	LOG(INFO) << "PostHandler : File \""<< target << "\" created successfully.";
+	LOG(INFO) << "PostHandler : File \"" << target <<
+ "\" created successfully.";
 	return response;
 }
