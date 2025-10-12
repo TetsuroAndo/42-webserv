3920af8e21d676541866d75840871645e2fcf49a
Tomoki Sato <tomokirenrakuyo@gmail.com>
2025-10-12 21:35:27 +0900

    fix: リクエストパーサーミドルウェアから最大ボディサイズの設定を削除

--- src/Middleware/RequestParser/RequestParserMiddleware.cpp
+++ src/Middleware/RequestParser/RequestParserMiddleware.cpp
@@ -8,7 +8,6 @@ void RequestParserMiddleware::handle(PipelineContext &ctx,
 		return;
 	}
 
-	ctx.req->setMaxBodySize(ctx.conf.getMaxRequestBodySize());
 	const ParseResult result = ctx.parser.parse(*ctx.req, ctx.recvBuffer);
 
 	if (result == PARSE_COMPLETE) {
