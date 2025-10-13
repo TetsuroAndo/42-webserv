4bb4b39f93a34820d16f57be9088379184b91017
Tomoki Sato <tomokirenrakuyo@gmail.com>
2025-10-13 22:15:49 +0900

    refactor: SessionMiddlewareでセッションIDのログ出力をNULLチェック付きで修正

--- src/Middleware/PipelineRouter/Session/SessionMiddleware.cpp
+++ src/Middleware/PipelineRouter/Session/SessionMiddleware.cpp
@@ -56,12 +56,14 @@ void SessionMiddleware::handle(PipelineContext &ctx,
 	}
 	Session *currentSession = NULL;
 	if (token.empty() == false) {
-		 currentSession = manager.getSession(token);
-		LOG(INFO) << "Session ID :" << currentSession->getId();
+		currentSession = manager.getSession(token);
+		if (currentSession != NULL) {
+			LOG(INFO) << "Session ID: " << currentSession->getId();
+		}
 	}
 	if (NULL == currentSession) {
 		currentSession = manager.createSession();
-		LOG(INFO) << "Created session " << currentSession->getId();
+		LOG(INFO) << "Created session: " << currentSession->getId();
 	}
 	ctx.session = currentSession;
 
