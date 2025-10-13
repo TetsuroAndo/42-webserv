bffeb5a62b62d0d2d4f44967a94f6dc6752e7b35
Tomoki Sato <tomokirenrakuyo@gmail.com>
2025-10-13 18:48:56 +0900

    refactor: SessionMiddlewareにセッション処理のTODOを追加

--- src/Middleware/PipelineRouter/Session/SessionMiddleware.cpp
+++ src/Middleware/PipelineRouter/Session/SessionMiddleware.cpp
@@ -12,6 +12,15 @@ SessionMiddleware::~SessionMiddleware() {}
 void SessionMiddleware::handle(PipelineContext &ctx,
 							   MiddlewareProcessor *proc) {
 	(void)ctx;
+	// TODO: ヘッダーがセッション情報を持ってるか見る
+	std::string reqCookieRaw = ctx.req->getHeader("");
+	// TODO: ヘッダーのCookie情報をパースする
+	// TODO: セッションが有効なものかを確認する
+	// TODO: セッションがなければ新しく作る
+	// TODO: セッション情報を保存する
+	// TODO: セッション情報をレスポンスにつける。
+
+
 	if (proc) {
 		proc->next(ctx);
 	}
