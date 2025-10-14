83dfb1f98b8964b2cfc322b011338354db9276ba
TetsuroAndo <tetsuro997@gmail.com>
2025-10-14 15:26:42 +0900

    全configファイルにフォーマットを実行

--- src/Config/ConfigParser.hpp
+++ src/Config/ConfigParser.hpp
@@ -8,26 +8,28 @@ class Node;
 
 class ConfigParser {
 private:
-    Config* _config;
+	Config *_config;
 
 public:
-    static const std::set<std::string> VALID_SERVER_KEYS;
-    static const std::set<std::string> VALID_LISTEN_KEYS;
-    static const std::set<std::string> VALID_REDIRECT_KEYS;
-    static const std::set<std::string> VALID_LOCATION_KEYS;
-    static const std::set<std::string> VALID_ACCESS_LOG_KEYS;
-    static const std::set<std::string> VALID_ERROR_LOG_KEYS;
-    static const std::set<std::string> VALID_DISABLED_ACCESS_LOG_KEYS;
-    static const std::set<std::string> VALID_DISABLED_ERROR_LOG_KEYS;
-    static const std::set<std::string> VALID_ALLOWED_METHODS;
+	static const std::set<std::string> VALID_SERVER_KEYS;
+	static const std::set<std::string> VALID_LISTEN_KEYS;
+	static const std::set<std::string> VALID_REDIRECT_KEYS;
+	static const std::set<std::string> VALID_LOCATION_KEYS;
+	static const std::set<std::string> VALID_ACCESS_LOG_KEYS;
+	static const std::set<std::string> VALID_ERROR_LOG_KEYS;
+	static const std::set<std::string> VALID_DISABLED_ACCESS_LOG_KEYS;
+	static const std::set<std::string> VALID_DISABLED_ERROR_LOG_KEYS;
+	static const std::set<std::string> VALID_ALLOWED_METHODS;
 
-    static void validateKeys(const Node *node, const std::set<std::string> &validKeys, const std::string &context);
+	static void validateKeys(const Node *node,
+							 const std::set<std::string> &validKeys,
+							 const std::string &context);
 
 public:
-    ConfigParser(Config* config);
-    ~ConfigParser();
+	ConfigParser(Config *config);
+	~ConfigParser();
 
-    void parseServer(const Node* serverNode);
-    void parseListens(const Node *node);
-    void parseRedirects(Node *node);
+	void parseServer(const Node *serverNode);
+	void parseListens(const Node *node);
+	void parseRedirects(Node *node);
 };
