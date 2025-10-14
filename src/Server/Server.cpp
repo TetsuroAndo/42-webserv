60066457e51f35aac161372fcc3b5bbe5457cd0c
Tomoki Sato <tomokirenrakuyo@gmail.com>
2025-10-14 12:11:14 +0900

    fix: セッションIDをログに追加

--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -195,7 +195,7 @@ void Server::handleClientRead(const int clientFd) {
 	if (ctx->parser.isComplete() || ctx->parser.getErrorCode() != 0) {
 		AccessLogger::getInstance().log(ctx->req, ctx->res, client->getIp(),
 										client->getPort(),
-										""); // TODO: セッションIDをここに
+										ctx->session->getId());
 		const std::string responseStr = ResponseBuilder::build(*ctx->res);
 		if (!responseStr.empty()) {
 			client->getSocket()->setSendBuffer(
