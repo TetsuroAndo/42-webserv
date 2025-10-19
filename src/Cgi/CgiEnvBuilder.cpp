2c32c3a3c3cb5a46cd07e43b4bdc44e78e3f595e
Tomoki Sato <tomokirenrakuyo@gmail.com>
2025-10-19 22:44:00 +0900

    wip(CgiEnvBuilder): 環境変数の構築に必要な要素を追加

--- src/Cgi/CgiEnvBuilder.cpp
+++ src/Cgi/CgiEnvBuilder.cpp
@@ -1,7 +1,10 @@
 #include "CgiEnvBuilder.hpp"
 
+#include "../Lib/StringOps/StringOps.hpp"
+#include "../Server/Client.hpp"
+
 namespace {
-const std::vector< std::string >
+std::vector< std::string >
 createEnvpArray(const std::map< std::string, std::string > &envMap) {
 	std::vector< std::string > envpStrs;
 	envpStrs.reserve(envMap.size());
@@ -26,23 +29,37 @@ createEnvpArray(const std::map< std::string, std::string > &envMap) {
 std::vector< std::string > CgiEnvBuilder::build(const PipelineContext &ctx,
 												const std::string &scriptPath) {
 	const Config &c = ctx.conf;
-	const HttpRequest &req = ctx.req;
-	const HttpResponse &res = ctx.res;
+	const HttpRequest &req = *ctx.req;
 	std::map< std::string, std::string > envMap;
 
-	envMap["GATEWAY_INTERFACE"] = ;
-	envMap["SERVER_PROTOCOL"] = ;
-	envMap["REQUEST_METHOD"] = ;
-	envMap["SCRIPT_FILENAME"] = ;
-	envMap["SCRIPT_NAME"] = ;
-	envMap["SERVER_SOFTWARE"] = ;
-	envMap["SERVER_NAME"] = ;
-	envMap["SERVER_PORT"] = ;
-	envMap["REMOTE_ADDR"] = ;
-	envMap["REMOTE_PORT"] = ;
-	envMap["QUERY_STRING"] = ;
-	envMap["CONTENT_TYPE"] = ;
-	envMap["CONTENT_LENGTH"] = ;
+	// TODO: 足りない要素をパーサーで解析して埋める
 
+	std::vector< std::string > Authorization =
+		StringOps::split(req.getHeader("Authorization"), " ");
+	std::string remoteUser = "";
+	if (1 <= Authorization.size()) {
+		remoteUser = Authorization[1]; // TODO:BASE64でデコードする
+	}
+
+	envMap["AUTH_TYPE"] =
+		StringOps::split(req.getHeader("Authorization"), " ")[0];
+	envMap["CONTENT_LENGTH"] = req.getBody().size();
+	envMap["CONTENT_TYPE"] = req.getHeader("Content-Type");
+	envMap["GATEWAY_INTERFACE"] = "CGI/1.1";
+	envMap["PATH_INFO"] = "";		// Locationsのroot+ファイル名
+	envMap["PATH_TRANSLATED"] = ""; // リクエストのURIを全文 (文字列操作で作る)
+	envMap["QUERY_STRING"] = "";	// リクエストの?以降をここに
+	envMap["REMOTE_ADDR"] = ctx.ownerClient.getIp();
+	envMap["REMOTE_HOST"] =
+		""; // 空文字で登録(digコマンドを実行する必要あるため)
+	envMap["REMOTE_IDENT"] = ctx.session->getId();
+	envMap["REMOTE_USER"] = remoteUser;
+	envMap["REQUEST_METHOD"] = req.getMethod();
+	envMap["SCRIPT_NAME"] = ""; // まっさらなCGIのファイル名
+	envMap["SERVER_NAME"] = ""; // Locationsで指定されるIPアドレス
+	envMap["SERVER_PORT"] = ""; // Locationsのうち、scriptPathが属す場所のポート
+	envMap["SERVER_PROTOCOL"] = c.getAppInfo().httpProtocolVersion;
+	envMap["SERVER_SOFTWARE"] = ctx.conf.getAppInfo().softwareName;
+	envMap["REMOTE_PORT"] = StringOps::toString(ctx.ownerClient.getPort());
 	return createEnvpArray(envMap);
 }
