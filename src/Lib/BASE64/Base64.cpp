ae790dc2f200e2358dc2400bdab25b5bdb063cf7
Tomoki Sato <tomokirenrakuyo@gmail.com>
2025-10-22 17:37:43 +0900

    wip(Base64): Base64エンコードおよびデコード関数を追加

---
+++ src/Lib/BASE64/Base64.cpp
@@ -0,0 +1,11 @@
+#include "Base64.hpp"
+
+std::string Base64::encode(const std::string &input) {
+	(void)input;
+	return "";
+}
+
+std::string Base64::decode(const std::string &input) {
+	(void)input;
+	return "";
+}
---
+++ src/Lib/BASE64/Base64.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+namespace Base64 {
+std::string encode(const std::string &input);
+std::string decode(const std::string &input);
+}; // namespace Base64
