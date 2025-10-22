0cd0b5d8e726c24dee1823dd3be717bdfc29d4c8
Tomoki Sato <tomokirenrakuyo@gmail.com>
2025-10-22 22:15:12 +0900

    feat(CgiEnvBuilder): リクエストパスからクエリ文字列とファイル名を抽出する関数を追加

--- src/Cgi/CgiEnvBuilder.cpp
+++ src/Cgi/CgiEnvBuilder.cpp
@@ -1,5 +1,7 @@
 #include "CgiEnvBuilder.hpp"
 
+#include "../Handler/HandlerUtil.hpp"
+#include "../Lib/Logger/Log.hpp"
 #include "../Lib/StringOps/StringOps.hpp"
 #include "../Server/Client.hpp"
 #include "../lib/Base64/Base64.hpp"
@@ -32,6 +34,37 @@ std::string fullURI(const std::string &version, const std::string &ip,
 	return result;
 }
 
+std::string queryString(const std::string &scriptPath) {
+	std::string trimmed = StringOps::trim(scriptPath);
+	return trimmed.substr(trimmed.find('?'), trimmed.size());
+}
+
+std::string fileName(const std::string &scriptPath) {
+	std::string trimmed = scriptPath;
+	trimmed.erase(0, trimmed.find_first_not_of(" \t\n\r"));
+	trimmed.erase(trimmed.find_last_not_of(" \t\n\r") + 1);
+
+	std::size_t pos = trimmed.find('?');
+	if (pos != std::string::npos)
+		trimmed = trimmed.substr(0, pos);
+	pos = trimmed.find_last_of('/');
+	const std::string name =
+		(pos != std::string::npos) ? trimmed.substr(pos + 1) : trimmed;
+
+	return "/" + name;
+}
+
+std::string pathInfo(const std::string &input) {
+	std::string trimmed = input;
+	trimmed.erase(0, trimmed.find_first_not_of(" \t\n\r"));
+	trimmed.erase(trimmed.find_last_not_of(" \t\n\r") + 1);
+
+	std::size_t pos = trimmed.find('?');
+	if (pos != std::string::npos)
+		trimmed = trimmed.substr(0, pos);
+	return trimmed;
+}
+
 } // namespace
 
 /**
@@ -40,17 +73,18 @@ std::string fullURI(const std::string &version, const std::string &ip,
  * 参考: https://4judgement.github.io/rfc-translater/html/rfc3875.html
  *
  * @param ctx リクエストのコンテキスト
- * @param scriptPath 実行するCGIスクリプトのフルパスと'?'以降のクエリ
+ * @param requestedPath リクエストのパス
  * @return "KEY=VALUE"形式のvector
  */
-std::vector< std::string > CgiEnvBuilder::build(const PipelineContext &ctx,
-												const std::string &scriptPath) {
+std::vector< std::string >
+CgiEnvBuilder::build(const PipelineContext &ctx,
+					 const std::string &requestedPath) {
 	const Config &c = ctx.conf;
 	const HttpRequest &req = *ctx.req;
 	std::map< std::string, std::string > envMap;
 
 	// TODO: 足りない要素をパーサーで解析して埋める
-	(void)scriptPath;
+	std::string fullPath = HandlerUtil::resolvePath(requestedPath, c);
 
 	const std::vector< std::string > Authorization =
 		StringOps::split(req.getHeader("Authorization"), " ");
@@ -59,26 +93,42 @@ std::vector< std::string > CgiEnvBuilder::build(const PipelineContext &ctx,
 		remoteUser = Base64::decode(Authorization[1]);
 	}
 
+	Location loc = c.getLocation(requestedPath);
+
 	envMap["AUTH_TYPE"] =
 		StringOps::split(req.getHeader("Authorization"), " ")[0];
 	envMap["CONTENT_LENGTH"] = req.getBody().size();
 	envMap["CONTENT_TYPE"] = req.getHeader("Content-Type");
 	envMap["GATEWAY_INTERFACE"] = "CGI/1.1";
-	envMap["PATH_INFO"] = ""; // Locationsのroot+ファイル名
+	envMap["PATH_INFO"] = pathInfo(requestedPath); // Locationsのroot+ファイル名
 	envMap["PATH_TRANSLATED"] =
 		::fullURI(c.getAppInfo().httpProtocolVersion, "", "",
-				  "");			 // リクエストのURIを全文 (文字列操作で作る)
-	envMap["QUERY_STRING"] = ""; // リクエストの?以降をここに
+				  ""); // リクエストのURIを全文 (文字列操作で作る)
+	envMap["QUERY_STRING"] =
+		queryString(requestedPath); // リクエストの?以降をここに
 	envMap["REMOTE_ADDR"] = ctx.ownerClient.getIp();
 	envMap["REMOTE_HOST"] = ""; // 空文字で登録
 	envMap["REMOTE_IDENT"] = ctx.session->getId();
 	envMap["REMOTE_USER"] = remoteUser;
 	envMap["REQUEST_METHOD"] = req.getMethod();
-	envMap["SCRIPT_NAME"] = ""; // まっさらなCGIのファイル名
-	envMap["SERVER_NAME"] = ""; // Locationsで指定されるIPアドレス
-	envMap["SERVER_PORT"] = ""; // Locationsのうち、scriptPathが属す場所のポート
+	envMap["SCRIPT_NAME"] =
+		fileName(requestedPath); // まっさらなCGIのファイル名
+	envMap["SERVER_NAME"] =
+		c.getListens()[0]
+			.interface; // Locationsで指定されるIPアドレス(0番目で固定)
+	envMap["SERVER_PORT"] =
+		c.getListens()[0]
+			.port; // Locationsのうち、scriptPathが属す場所のポート(0番目で固定)
 	envMap["SERVER_PROTOCOL"] = c.getAppInfo().httpProtocolVersion;
 	envMap["SERVER_SOFTWARE"] = ctx.conf.getAppInfo().softwareName;
 	envMap["REMOTE_PORT"] = StringOps::toString(ctx.ownerClient.getPort());
+
+	LOG(DEBUG) << "Env map created: ";
+	for (std::map< std::string, std::string >::const_iterator it =
+			 envMap.begin();
+		 it != envMap.end(); ++it) {
+		LOG(DEBUG) << "  " << it->first << "=" << it->second;
+	}
+
 	return createEnvpArray(envMap);
 }
--- src/Cgi/CgiEnvBuilder.hpp
+++ src/Cgi/CgiEnvBuilder.hpp
@@ -9,11 +9,11 @@ public:
 	/**
 	 * @brief PipelineContextからCGI環境変数のリストを生成する
 	 * @param ctx リクエストのコンテキスト
-	 * @param scriptPath 実行するCGIスクリプトのフルパス
+	 * @param requestedPath 実行するCGIスクリプトのフルパス
 	 * @return "KEY=VALUE"形式の文字列配列
 	 */
 	static std::vector< std::string > build(const PipelineContext &ctx,
-											const std::string &scriptPath);
+											const std::string &requestedPath);
 
 private:
 	CgiEnvBuilder();
