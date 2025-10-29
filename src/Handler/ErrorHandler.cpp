9930cfea5fc85ba7ad001cf9f22d417f135a6c72
TetsuroAndo <tetsuro997@gmail.com>
2025-10-30 03:47:54 +0900

    delete handlerUtil.h

--- src/Handler/ErrorHandler.cpp
+++ src/Handler/ErrorHandler.cpp
@@ -3,7 +3,6 @@
 #include "../Http/Core/HttpStatus.hpp"
 #include "../Http/Resolver/RequestResolver.hpp"
 #include "../Lib/Logger/Log.hpp"
-#include "HandlerUtil.hpp"
 #include <fstream>
 #include <sys/stat.h>
 
