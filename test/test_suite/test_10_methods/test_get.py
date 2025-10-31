ed15d20b25c8be1a8886ce6dacbd9b260c8426f6
Tomoki Sato <tomokirenrakuyo@gmail.com>
2025-10-31 21:31:59 +0900

    fix(test):  test/test_suite/test_10_methods/を最新のConfigに合わせた。

--- test/test_suite/test_10_methods/test_get.py
+++ test/test_suite/test_10_methods/test_get.py
@@ -30,12 +30,18 @@ class TestGET:
 
     @pytest.mark.config("valid/config_autoindex_on.yaml")
     def test_autoindex_on(self, managed_server):
-        url = f"{managed_server['base_url']}/autoindex_test_dir/"
+        url = f"{managed_server['base_url']}/"
         response = requests.get(url)
         assert response.status_code == 200
 
     @pytest.mark.config("valid/config_autoindex_off.yaml")
     def test_autoindex_off_forbidden(self, managed_server):
-        url = f"{managed_server['base_url']}/no_autoindex_dir/"
+        url = f"{managed_server['base_url']}/"
         response = requests.get(url)
         assert response.status_code == 403
+
+    @pytest.mark.config("valid/config_basic_get.yaml")
+    def test_get_method_not_allowed(self, managed_server):
+        url = f"{managed_server['base_url']}/not_allow_get/hello.txt"
+        response = requests.get(url)
+        assert response.status_code == 405
