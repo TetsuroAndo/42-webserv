0af26114162b4e3a2bca0df0273a5df85009b0f0
Tomoki Sato <tomokirenrakuyo@gmail.com>
2025-11-01 14:09:09 +0900

    fix(test): テストを修正

--- test/test_suite/test_10_methods/test_get.py
+++ test/test_suite/test_10_methods/test_get.py
@@ -57,10 +57,13 @@ class TestGET:
 
     @pytest.mark.config("valid/config_autoindex_with_index_root.yaml")
     def test_autoindex_on_with_index_file_root(self, managed_server):
+        # rootにautoindexとindexが両方設定されている場合、
+        # リクエストが同一のものになり区別できないため、
+        # indexが優先されているかを確認します。
         url = f"{managed_server['base_url']}/"
         response = requests.get(url)
         assert response.status_code == 200
-        assert "<html><head><title>Index of " in response.text
+        assert "<html><body><h1>Welcome!</h1></body></html>" in response.text
 
     @pytest.mark.config("valid/config_autoindex_off.yaml")
     def test_autoindex_off_forbidden(self, managed_server):
