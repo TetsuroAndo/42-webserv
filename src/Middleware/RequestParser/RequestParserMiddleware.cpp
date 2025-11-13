ffaaf1d52b4a4bde5bbd0ae84c378b52c9199f97
Tomoki Sato <tomokirenrakuyo@gmail.com>
2025-11-13 23:00:02 +0900

    fix(RequestParserMiddleware): リクエストボディサイズの制限と、パーサーの間で発生していた仕様ずれによるバグを修正

--- src/Middleware/RequestParser/RequestParserMiddleware.cpp
+++ src/Middleware/RequestParser/RequestParserMiddleware.cpp
@@ -7,25 +7,34 @@
 
 void RequestParserMiddleware::handle(PipelineContext &ctx,
 									 MiddlewareProcessor *proc) {
-
-	// パーサーが初期状態の場合、まず「緩い」上限を設定する
-	// これにより、ヘッダーパース中にバッファが大きくなりすぎても
-	// _bodyParserが（もし動作しても）誤ってエラーを出すのを防ぐ
 	if (ctx.parser.getState() == RequestParser::STATE_REQUEST_LINE) {
 		unsigned int biggestSize = 0;
 		if (ctx.conf.hasBiggestMaxRequestBodySize()) {
 			biggestSize = ctx.conf.getBiggestMaxRequestBodySize();
 		}
-		unsigned int globalSize = ctx.conf.getMaxRequestBodySize();
+		const unsigned int globalSize = ctx.conf.getMaxRequestBodySize();
 		ctx.req.setMaxBodySize(std::max(biggestSize, globalSize));
 	}
 
-	const ParseResult result = ctx.parser.parse(ctx.req, ctx.recvBuffer);
+	ParseResult result = PARSE_HEADERS_COMPLETE;
+	while (PARSE_HEADERS_COMPLETE == result) {
+		result = ctx.parser.parse(ctx.req, ctx.recvBuffer);
+		if (PARSE_HEADERS_COMPLETE == result) {
+			const Location loc = ctx.conf.getLocation(ctx.req.getPath());
+			if (loc.maxRequestBodySize != -1) {
+				const std::size_t effectiveLimit =
+					static_cast< std::size_t >(loc.maxRequestBodySize);
+				if (ctx.req.getBody().size() > effectiveLimit) {
+					ctx.req.setMaxBodySize(effectiveLimit);
+				}
+			}
+		}
+	}
 
 	switch (result) {
-	case PARSE_INCOMPLETE:
+	case PARSE_INCOMPLETE: {
 		return;
-
+	}
 	case PARSE_ERROR: {
 		const int code = ctx.parser.getErrorCode();
 		ctx.res.setStatusCode(code);
@@ -35,25 +44,14 @@ void RequestParserMiddleware::handle(PipelineContext &ctx,
 		}
 		return;
 	}
-
 	case PARSE_HEADERS_COMPLETE: {
-		const Location loc = ctx.conf.getLocation(ctx.req.getPath());
-		// このリクエストに適用されるべき「有効な」リミットを決定する
-		std::size_t effectiveLimit;
-		if (loc.maxRequestBodySize == -1) {
-			effectiveLimit = ctx.conf.getMaxRequestBodySize();
-		} else {
-			effectiveLimit = static_cast< std::size_t >(loc.maxRequestBodySize);
-		}
-		ctx.req.setMaxBodySize(effectiveLimit);
+		// 呼ばれない
 		return;
 	}
-
 	case PARSE_COMPLETE: {
 		if (proc) {
 			proc->next(ctx);
 		}
-		return;
 	}
 	}
 }
