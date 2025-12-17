d224e034022e12afbb7d114e2dbaa087c63915c0
Tomoki Sato <tomokirenrakuyo@gmail.com>
2025-12-17 22:38:39 +0900

    tester wip

--- src/Handler/StaticFileHandler.cpp
+++ src/Handler/StaticFileHandler.cpp
@@ -178,7 +178,7 @@ HttpResponse StaticFileHandler::handle(PipelineContext &ctx) {
 			} else {
 				LOG(WARNING) << "Directory listing is disabled for"
 							 << attr("path", filePath);
-				res.setStatusCode(HttpStatus::FORBIDDEN);
+				res.setStatusCode(HttpStatus::NOT_FOUND);
 			}
 			return res;
 		}
