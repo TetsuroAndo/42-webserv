1eead01df426748c8285f78eedafa1f2c6fc6e8d
Tomoki Sato <tomokirenrakuyo@gmail.com>
2026-01-07 14:09:44 +0900

    refactor(Server): sigchildFdの処理が不要なため削除しました
    役割がcleanupFinishedWorkersと被ってる

--- src/Server/Server.hpp
+++ src/Server/Server.hpp
@@ -35,7 +35,6 @@ private:
 	CgiManager _cgiManager;
 	TimeoutManager _timeoutManager;
 	SocketsManager _socketsManager;
-	int _sigchldPipe[2];
 	std::map< int, Socket * > _listenSockets;
 	std::map< int, Client * > _clients;
 	PipelineRouteBuilder _builder;
@@ -43,9 +42,7 @@ private:
 	EventManager _eventManager;
 
 	void setupListenSockets();
-	void setupSignalPipe();
 	void handleNewConnection(int listenFd);
-	void handleSigchldEvent();
 
 	std::string getSessionId(const PipelineContext *ctx) const;
 };
